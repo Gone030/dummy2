eac4fd83e680879d7eb643cab3d56a2f3b7b5dec
Gone030 <gjkjw_030@naver.com>
2024-05-16 19:16:50 +0900

    Launch File edit

--- dummy2_gazebo/launch/bringup.launch.py
+++ dummy2_gazebo/launch/bringup.launch.py
@@ -1,8 +1,29 @@
 import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     pkg_dummy2 = get_package_share_directory('dummy2_gazebo')
+
+    world = LaunchConfiguration('world')
+
+    declere_world_config = DeclareLaunchArgument(
+        'world',
+        default_value=[os.path.join(pkg_dummy2, 'worlds', 'empty.world'), ''],
+        description='world file'
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'world': world}.items(),
+    )
+    ld = LaunchDescription()
+    ld.add_action(declere_world_config)
+    ld.add_action(gazebo)
+
+    return ld
--- dummy2_gazebo/launch/description.launch.py
+++ dummy2_gazebo/launch/description.launch.py
@@ -3,35 +3,43 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch.actions import TimerAction
 import xacro
 
 
 def generate_launch_description():
-    xacro_file = os.path.join(get_package_share_directory('test_gazebo'), 'urdf/', 'base.urdf.xacro')
+    xacro_file = os.path.join(get_package_share_directory('dummy2_gazebo'), 'urdf/', 'base.urdf.xacro')
 
     robot_description_config = xacro.process_file(xacro_file)
     robot_desc = robot_description_config.toxml()
 
-    return LaunchDescription([
+    spawn_car = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-topic", "/robot_description", "-entity", "dummy2"]
+    )
+
+    robot_state_pub = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        parameters=[{"robot_description": robot_desc}],
+        output="screen"
+    )
+    ld =  LaunchDescription([
         DeclareLaunchArgument(
-            name='use_sim_time',
+            'use_sim_time',
             default_value='true',
-            description='use sim time'
+            description='using sim time'
         ),
-
-
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=["-topic", "robot_description", "-entity", "test1"] #, "-x", "-2.5", "-y", "1.0", "-z", "0.2"]
+        TimerAction(
+            period=0.0 ,
+            actions=[spawn_car]
         ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[
-                {"robot_description": robot_desc}
-            ]
+        TimerAction(
+            period=0.0,
+            actions=[robot_state_pub]
         )
     ])
+
+    return ld
